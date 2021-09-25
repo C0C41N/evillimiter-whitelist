c838e36679ebcd24e30893bd9d90e746933ae565
C0C41N <0ct41n@gmail.com>
2021-09-25 19:43:24 -0400

    init

---
+++ parse.sh
@@ -0,0 +1,62 @@
+#! /usr/bin/bash
+
+whitelist=()
+arrId=()
+arrIp=()
+arrMac=()
+
+while read line; do
+	whitelist+=($line)
+done < whitelist.txt
+
+while read line; do
+  start=${line:0:13}
+	if [ "$start" == "(0x(B [93m" ]
+	then
+		# ID
+
+		id=${line:13:2}
+
+		if [ "${id:1:1}" == "" ]
+		then
+			id=${id:0:1}
+		fi
+
+		# IP
+
+		ip=${line:28:15}
+
+		if [ "${ip:13:2}" == "  " ]
+		then
+			ip=${ip:0:13}
+		fi
+
+		if [ "${ip:14:1}" == " " ]
+		then
+			ip=${ip:0:14}
+		fi
+
+		# MAC
+
+		mac=${line:51:17}
+		# echo ""
+		# echo "$mac"
+		# echo ""
+
+		for i in "${whitelist[@]}"
+		do
+		  if [ "$i" == "$mac" ]
+			then
+				arrMac+=($mac)
+				arrId+=($id)
+				arrIp+=($ip)
+			fi
+		done
+
+	fi
+done < hosts.txt
+
+printf '%s\n' "${arrId[@]}"
+# printf '%s\n' "${arrIp[@]}"
+# printf '%s\n' "${arrMac[@]}"
+echo "000end"
